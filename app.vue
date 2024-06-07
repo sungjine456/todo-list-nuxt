57321d44bc589bdb042590b86aacbf96e101fc45
sungjine <sungjine456@gmail.com>
2024-06-07 16:48:15 +0900

    간다한 Todo List의 CRUD 구현

--- app.vue
+++ app.vue
@@ -1,5 +1,22 @@
 <template>
-  <div>
-    <NuxtWelcome />
-  </div>
+  <NuxtPage />
 </template>
+
+<style>
+html {
+  height: 100%;
+}
+
+body {
+  height: 100%;
+  margin: 0;
+}
+
+#__nuxt {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  width: 100%;
+  height: 100%;
+}
+</style>
---
+++ pages/index.vue
@@ -0,0 +1,104 @@
+<template>
+  <div>
+    <div class="form">
+      <input v-model="value" />
+      <button @click="add">추가</button>
+    </div>
+    <div v-if="!list.length">
+      <p>비었습니다.</p>
+    </div>
+    <div v-for="(item, i) in list" :key="i" class="item">
+      <template v-if="indexToUpdate === i">
+        <input v-model="target" />
+        <div>
+          <button @click="update(i)">확인</button>
+          <button @click="indexToUpdate = -1">취소</button>
+        </div>
+      </template>
+      <template v-else>
+        <span @click="showUpdate(i)">{{ item }}</span>
+        <div>
+          <button @click="showUpdate(i)">수정</button>
+          <button @click="remove(i)">삭제</button>
+        </div>
+      </template>
+    </div>
+  </div>
+</template>
+
+<script setup lang="ts">
+const target = ref("");
+const indexToUpdate = ref(-1);
+const value = ref("");
+const list = ref([]);
+
+const add = () => {
+  list.value.push(value.value);
+  value.value = "";
+
+  syncStorage();
+};
+
+const remove = (index) => {
+  list.value.splice(index, 1);
+
+  syncStorage();
+};
+
+const update = (index) => {
+  list.value.splice(index, 1, target.value);
+  indexToUpdate.value = -1;
+  target.value = "";
+
+  syncStorage();
+};
+
+const showUpdate = (index) => {
+  indexToUpdate.value = index;
+  target.value = list.value[index];
+};
+
+const syncStorage = () => {
+  localStorage.setItem("list", JSON.stringify(list.value));
+};
+
+onMounted(() => {
+  list.value = JSON.parse(localStorage.getItem("list")) ?? [];
+});
+</script>
+
+<style scoped>
+.form {
+  margin-bottom: 10px;
+}
+
+.form input {
+  height: 19px;
+  margin-right: 5px;
+}
+
+.form button {
+  height: 25px;
+}
+
+.item {
+  display: flex;
+  justify-content: space-between;
+}
+
+.item span,
+.item input {
+  width: 125px;
+}
+
+.item span {
+  cursor: pointer;
+  border: solid white 1px;
+  border-radius: 3px;
+}
+
+.item span:hover {
+  text-decoration: underline;
+  border: solid black 1px;
+}
+</style>
