2fa95ea0f522376e09c8b85523d27b062a9cfb86
sungjine <sungjine456@gmail.com>
2024-06-12 16:59:06 +0900

    달력 컴포넌트의 코드 개선

--- components/CalendarComponent.vue
+++ components/CalendarComponent.vue
@@ -1,9 +1,9 @@
 <template>
   <div class="calendar">
     <div class="handler">
-      <button @click="calculateDate(-1)">&lt;</button>
-      {{ year }}년 {{ month }}월
-      <button @click="calculateDate(1)">&gt;</button>
+      <button @click="calculateCalender(-1)">&lt;</button>
+      {{ targetDate.format("YYYY년 MM월") }}
+      <button @click="calculateCalender(1)">&gt;</button>
     </div>
     <table>
       <thead>
@@ -17,16 +17,18 @@
             :class="{
               gray:
                 (idx === 0 && day >= lastMonthStart) ||
-                (dates.length - 1 === idx && nextMonthStart > day),
+                (dates.length - 1 === idx && day < 8),
               red: secondIdx === 0,
               blue: secondIdx === 6,
               today:
-                day === today && month === currentMonth && year === currentYear,
+                day === now.date() &&
+                targetDate.month() === now.month() &&
+                targetDate.year() === now.year(),
               target:
                 !(
                   (idx === 0 && day >= lastMonthStart) ||
-                  (dates.length - 1 === idx && nextMonthStart > day)
-                ) && day === target
+                  (dates.length - 1 === idx && day < 8)
+                ) && day === targetDate.date()
             }"
             @click="(e) => changeTarget(e.target as Element, day)"
           >
@@ -39,63 +41,30 @@
 </template>
 
 <script lang="ts" setup>
+import dayjs, { Dayjs } from "dayjs";
+
 const emit = defineEmits(["changedTarget"]);
 
 const days = ["일", "월", "화", "수", "목", "금", "토"];
+const now = dayjs();
+
 const dates = ref<number[][]>([]);
-const currentYear = ref<number>(0);
-const currentMonth = ref<number>(0);
-const year = ref<number>(0);
-const month = ref<number>(0);
+const targetDate = ref<Dayjs>(now);
 const lastMonthStart = ref<number>(0);
-const nextMonthStart = ref<number>(0);
-const today = ref<number>(0);
-const target = ref<number>(0);
 
-const now = new Date();
-currentYear.value = now.getFullYear();
-currentMonth.value = now.getMonth() + 1;
-year.value = currentYear.value;
-month.value = currentMonth.value;
-today.value = now.getDate();
-target.value = today.value;
+const calculateCalender = (month: number = 0) => {
+  targetDate.value = targetDate.value.add(month, "month");
 
-const calculateDate = (arg: number = 0) => {
-  if (arg < 0) {
-    month.value -= 1;
-  } else if (arg === 1) {
-    month.value += 1;
-  }
+  const thisMonth = targetDate.value.set("date", 1);
+  const previousMonth = targetDate.value.add(-1, "month");
 
-  if (month.value === 0) {
-    year.value -= 1;
-    month.value = 12;
-  } else if (month.value > 12) {
-    year.value += 1;
-    month.value = 1;
-  }
-
-  const [monthFirstDay, monthLastDate, lastMonthLastDate] = getFirstDayLastDate(
-    year.value,
-    month.value
+  dates.value = getMonthOfDays(
+    thisMonth.day(),
+    thisMonth.daysInMonth(),
+    previousMonth.daysInMonth()
   );
-  dates.value = getMonthOfDays(monthFirstDay, monthLastDate, lastMonthLastDate);
-};
 
-const getFirstDayLastDate = (year: number, month: number) => {
-  const firstDay = new Date(year, month - 1, 1).getDay();
-  const lastDate = new Date(year, month, 0).getDate();
-  let lastYear = year;
-  let lastMonth = month - 1;
-
-  if (month === 1) {
-    lastMonth = 12;
-    lastYear -= 1;
-  }
-
-  const prevLastDate = new Date(lastYear, lastMonth, 0).getDate();
-
-  return [firstDay, lastDate, prevLastDate];
+  emit("changedTarget", dayjs(targetDate.value));
 };
 
 const getMonthOfDays = (
@@ -103,42 +72,33 @@ const getMonthOfDays = (
   monthLastDate: number,
   prevMonthLastDate: number
 ): number[][] => {
-  let day: number = 1;
-  let prevDay: number = prevMonthLastDate - monthFirstDay + 1;
   const dates: number[][] = [];
   let weekOfDays: number[] = [];
 
-  while (day <= monthLastDate) {
-    if (day === 1) {
-      for (let j = 0; j < monthFirstDay; j += 1) {
-        if (j === 0) lastMonthStart.value = prevDay;
+  lastMonthStart.value = prevMonthLastDate - monthFirstDay + 1;
 
-        weekOfDays.push(prevDay);
-        prevDay += 1;
-      }
-    }
+  for (let i = lastMonthStart.value; i <= prevMonthLastDate; i++) {
+    weekOfDays.push(i);
+  }
 
-    weekOfDays.push(day);
+  for (let i = 1; i <= monthLastDate; i++) {
+    weekOfDays.push(i);
 
     if (weekOfDays.length === 7) {
       dates.push(weekOfDays);
       weekOfDays = [];
     }
-
-    day += 1;
   }
 
   const len = weekOfDays.length;
 
   if (len > 0 && len < 7) {
-    for (let k = 1; k <= 7 - len; k += 1) {
-      weekOfDays.push(k);
+    for (let i = 1; i <= 7 - len; i++) {
+      weekOfDays.push(i);
     }
   }
 
   if (weekOfDays.length > 0) dates.push(weekOfDays);
-
-  nextMonthStart.value = weekOfDays[0];
 
   return dates;
 };
@@ -146,19 +106,14 @@ const getMonthOfDays = (
 const changeTarget = (t: Element | null, day: number) => {
   // TODO: 이전 달이나 다음 달의 날짜를 누르면 해당 달로 이동하도록 수정하면 좋을 듯
   if (!t?.classList.contains("gray")) {
-    target.value = day;
+    targetDate.value = targetDate.value.set("date", day);
 
-    emit(
-      "changedTarget",
-      new Date(`${currentYear.value}-${currentMonth.value}-${day}`)
-    );
+    emit("changedTarget", dayjs(targetDate.value));
   }
 };
 
 onMounted(() => {
-  calculateDate();
-
-  emit("changedTarget", now);
+  calculateCalender();
 });
 </script>
 
